11ab61ab4f42ee92801ccbf4ad36076c278fa8cb
Brayan Jesús Gutiérrez Esparza <bhn_rock@hotmail.com>
2018-03-31 17:51:42 -0700

    Agregar tareas, listarlas y eliminarlas

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,23 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  tareas: string[] = [];
+  tarea: string;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  agregar(){
+    this.tareas.push(this.tarea);
+    this.tarea = "";
+  }
+
+  borrar(elemento){
+    var index = this.tareas.indexOf(elemento, 0);
+    if (index > -1){
+      this.tareas.splice(index, 1);
+    }
   }
 
 }
