5918e597e213f5fe23e29bfb72dbf8e7af9f77f1
AbrilHernandez <abyahernandezya@ittepic.edu.mx>
2018-04-08 17:57:09 -0600

    Implementación de Storage, metodos de agregar y eliminar del storage agregados

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { Storage } from '@ionic/storage';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,15 +9,22 @@ export class HomePage {
 
   tareas: string[] = [];
   tarea: string;
+  storage:Storage;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,storage:Storage) {
     this.tarea = "";
+    this.storage=storage;
   }
 
   agregar(){
     if(this.tarea != "" && this.tarea.trim.length == 0){
+      this.storage.set(this.tarea,'incompleta');//Guardar en storage, como key se guardara la tarea
+                                                //Su valor sera incompleta
       this.tareas.push(this.tarea);
       this.tarea = "";
+
+      //console.log(this.storage.ready()); //Conocer el estado del storage
+      //this.storage.get('estudiar').then((val) => {console.log(val);});//Obtener valor del storage
     }
   }
 
@@ -26,6 +33,7 @@ export class HomePage {
     if (index > -1){
       this.tareas.splice(index, 1);
     }
+    this.storage.remove(elemento); //eliminar del storage
   }
 
   completar(elemento: string){
